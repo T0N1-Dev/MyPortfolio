12ae2c66c6f1ab28e38fa4f812c6d7a56185d29b
T0N1-Dev <antonio.cg@avangenio.com>
2026-04-26 13:01:16 -0400

    fix(slider): replacing slider component by react-tech-slider

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import "../assets/css/pages/about.css";
 
 export const About = ({ aboutRef }) => {
@@ -7,68 +7,85 @@ export const About = ({ aboutRef }) => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrolledPercentage = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+      const scrolledPercentage =
+        (window.scrollY /
+          (document.documentElement.scrollHeight - window.innerHeight)) *
+        100;
       setIsScrolled(scrolledPercentage >= 50);
       setIsOutOf(scrolledPercentage >= 70);
     };
-  
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, [isScrolled, isOutOf]);
-  
 
   return (
     <>
-      <section className='container-about-me' ref={aboutRef}>
-        <div className='text-content-about-me'>
-          <h1><span>About</span> <span className='me'>Me</span></h1>
-          <p className='about-me-text'>
-            Hi! 👋 I'm Antonio, a passionate web developer with a degree in Computer Engineering from Universidad Tecnológica de La Habana. My curiosity for how things work led me to tech early on, and during my studies, I built and deployed a full web app as my final project, an experience that sparked my love for web development.
-
-            I'm eager to join a collaborative team where I can solve real problems, share ideas, and grow as both a developer and teammate. I value clean code, great user experiences, and continuous learning.
-
-            If you're looking for someone who’s not just skilled but also deeply committed to building great things let’s connect! 🚀
+      <section className="container-about-me" ref={aboutRef}>
+        <div className="text-content-about-me">
+          <h1>
+            <span>About</span> <span className="me">Me</span>
+          </h1>
+          <p className="about-me-text">
+            👋 I was born on December 12, 1994, in Havana, Cuba. From a young
+            age, I've always been passionate about video games and everything
+            related to technology, even though accessing it was nearly
+            impossible. At 15, I started learning the basics of computing, and
+            by 17, I already had a small office supply business. I decided to go
+            to university to fulfill a promise to my sister, and it was there
+            that I discovered my passion for coding. One of my biggest
+            motivations is learning new technologies and new methods to improve
+            and streamline software development. I love sharing ideas and
+            knowledge with other developers. I believe the best workplace is one
+            that allows me to constantly evolve. 🚀
           </p>
         </div>
-        <div className='about-me-images-container'>
-          <img 
+        <div className="about-me-images-container">
+          <img
             src="https://res.cloudinary.com/dmfs1od9n/image/upload/f_auto,q_auto/v1741279189/About_Me_s6rz76.png"
-            className='about-me-image'
+            className="about-me-image"
             alt="About Me"
-            width="400" height="400"
-            loading='lazy'
+            width="400"
+            height="400"
+            loading="lazy"
           />
-          <img 
-            src='https://res.cloudinary.com/dmfs1od9n/image/upload/f_auto,q_auto/v1741279168/Ball_Code_1_suixzv.png'
-            className='ball about-me-image-ballCode'
-            alt='Ball Code'
-            width="50" height="50"
-            loading='lazy'
+          <img
+            src="https://res.cloudinary.com/dmfs1od9n/image/upload/f_auto,q_auto/v1741279168/Ball_Code_1_suixzv.png"
+            className="ball about-me-image-ballCode"
+            alt="Ball Code"
+            width="50"
+            height="50"
+            loading="lazy"
           />
-          <img 
-            src='https://res.cloudinary.com/dmfs1od9n/image/upload/f_auto,q_auto/v1741019217/Ball_CSS_byzw6t.png'
-            className={`ball about-me-image-ballCSS ${isScrolled ? 'animate-ball' : ''}`} 
-            alt='Ball CSS'
-            width="50" height="50"
-            loading='lazy'
+          <img
+            src="https://res.cloudinary.com/dmfs1od9n/image/upload/f_auto,q_auto/v1741019217/Ball_CSS_byzw6t.png"
+            className={`ball about-me-image-ballCSS ${isScrolled ? "animate-ball" : ""}`}
+            alt="Ball CSS"
+            width="50"
+            height="50"
+            loading="lazy"
           />
-          <img 
-            src='https://res.cloudinary.com/dmfs1od9n/image/upload/f_auto,q_auto/v1741279170/Ball_HTML_1_jkjg9t.png'
-            className={`ball about-me-image-ballHTML ${isScrolled ? 'animate-ball' : ''}`}
-            alt='Ball HTML'
-            width="50" height="50"
-            loading='lazy'
+          <img
+            src="https://res.cloudinary.com/dmfs1od9n/image/upload/f_auto,q_auto/v1741279170/Ball_HTML_1_jkjg9t.png"
+            className={`ball about-me-image-ballHTML ${isScrolled ? "animate-ball" : ""}`}
+            alt="Ball HTML"
+            width="50"
+            height="50"
+            loading="lazy"
           />
-          <img 
-            src='https://res.cloudinary.com/dmfs1od9n/image/upload/f_auto,q_auto/v1741019217/Ball_JS_hwafir.png'
-            className={`ball about-me-image-ballJS ${isScrolled ? 'animate-ball' : ''}`} 
-            alt='Ball JS'
-            width="50" height="50"
-            loading='lazy'
+          <img
+            src="https://res.cloudinary.com/dmfs1od9n/image/upload/f_auto,q_auto/v1741019217/Ball_JS_hwafir.png"
+            className={`ball about-me-image-ballJS ${isScrolled ? "animate-ball" : ""}`}
+            alt="Ball JS"
+            width="50"
+            height="50"
+            loading="lazy"
           />
-          <div className={`glowing-circle ${isOutOf ? 'glowing-circle-animate' : ''}`}></div>
+          <div
+            className={`glowing-circle ${isOutOf ? "glowing-circle-animate" : ""}`}
+          ></div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import { useRef } from "react";
-import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
+import { Navbar } from "../components/Navbar";
 import { Home } from "../pages/Home";
-import { Projects } from "../pages/Projects"
-import { Slider } from "../components/Slider";
+import { Projects } from "../pages/Projects";
+// import { Slider } from "../components/Slider";
+import { Slider } from "react-tech-slider";
+import { techStack } from "../data/techStack";
 import { About } from "../pages/About";
 import { Contact } from "../pages/Contact";
-import { techStack } from "../data/techStack";
 
 export const AppRoutes = () => {
   const homeRef = useRef(null);
@@ -16,13 +17,24 @@ export const AppRoutes = () => {
 
   return (
     <>
-      <Navbar projectsRef={projectsRef} aboutRef={aboutRef} contactRef={contactRef} />
+      <Navbar
+        projectsRef={projectsRef}
+        aboutRef={aboutRef}
+        contactRef={contactRef}
+      />
       <Home homeRef={homeRef} contactRef={contactRef} />
       <Projects projectsRef={projectsRef} />
-      <Slider technologies={techStack} borderColor="gray" isPaused={false} durationMs={30000}/>
+      <div className="slider-container">
+        <Slider brandsList={techStack} borderColor="gray" durationMs={30000} />
+      </div>
       <About aboutRef={aboutRef} />
       <Contact contactRef={contactRef} />
-      <Footer homeRef={homeRef} aboutRef={aboutRef} contactRef={contactRef} projectsRef={projectsRef} />
+      <Footer
+        homeRef={homeRef}
+        aboutRef={aboutRef}
+        contactRef={contactRef}
+        projectsRef={projectsRef}
+      />
     </>
-  )
-}
+  );
+};
